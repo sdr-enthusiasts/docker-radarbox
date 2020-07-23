0d804d3f53b4ba628f4ced18240bdd235de774f9
Mike Nye <mike.nye@gmail.com>
2020-07-23 14:00:29 +0800

    Linting

--- build_dev.sh
+++ build_dev.sh
@@ -9,21 +9,7 @@ PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64"
 
 # Colours
 NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHTGRAY='\033[0;37m'
-DARKGRAY='\033[1;30m'
-LIGHTRED='\033[1;31m'
-LIGHTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-LIGHTBLUE='\033[1;34m'
 LIGHTPURPLE='\033[1;35m'
-LIGHTCYAN='\033[1;36m'
-WHITE='\033[1;37m'
 
 # Save current dir
 pushd .
@@ -39,10 +25,10 @@ echo -e "${LIGHTPURPLE}========== Copy mlat-client:armhf out of image ==========
 mkdir -p output
 rm -v ./output/*.deb || true
 MLAT_BUILDER_CONTAINER_ID=$(timeout 300s docker run -d --rm mlat-builder sleep 300)
-FILE_TO_COPY=$(docker exec ${MLAT_BUILDER_CONTAINER_ID} bash -c "ls /src/mlat-client*.deb")
-docker cp ${MLAT_BUILDER_CONTAINER_ID}:${FILE_TO_COPY} ./output/
-docker kill ${MLAT_BUILDER_CONTAINER_ID}
-docker rm ${MLAT_BUILDER_CONTAINER_ID} || true
+FILE_TO_COPY=$(docker exec "${MLAT_BUILDER_CONTAINER_ID}" bash -c "ls /src/mlat-client*.deb")
+docker cp "${MLAT_BUILDER_CONTAINER_ID}:${FILE_TO_COPY}" ./output/
+docker kill "${MLAT_BUILDER_CONTAINER_ID}"
+docker rm "${MLAT_BUILDER_CONTAINER_ID}" || true
 
 # Return to previous directory
 popd 
