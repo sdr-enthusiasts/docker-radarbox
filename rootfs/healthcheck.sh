8d323422de0abd482939aad06ad56614a0278716
Mike Nye <mike.nye@gmail.com>
2020-07-23 14:03:41 +0800

    Linting

--- rootfs/healthcheck.sh
+++ rootfs/healthcheck.sh
@@ -5,7 +5,7 @@ set -e
 EXITCODE=0
 
 TIMESTAMP_NOW=$(date +%s.%N)
-LASTLOG_PACKETS_SENT=$(cat /var/log/rbfeeder/current | grep "Packets sent in the last 30 seconds" | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
+LASTLOG_PACKETS_SENT=$(grep "Packets sent in the last 30 seconds" /var/log/rbfeeder/current | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
 LASTLOG_TIMESTAMP=$(date --date="$(echo "$LASTLOG_PACKETS_SENT" | cut -d '[' -f 1)" +%s.%N)
 LASTLOG_NUM_PACKETS_SENT=$(echo "$LASTLOG_PACKETS_SENT" | cut -d ']' -f 3 | cut -d ':' -f 2 | cut -d ',' -f 1 | tr -d " ")
 
