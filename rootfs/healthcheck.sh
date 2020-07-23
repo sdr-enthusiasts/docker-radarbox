ca7287ce0d7c798141e56eea6ca2760f2c1a46f8
Mike Nye <mike.nye@gmail.com>
2020-07-23 12:53:04 +0800

    Update healthcheck & startup script

--- rootfs/healthcheck.sh
+++ rootfs/healthcheck.sh
@@ -8,6 +8,7 @@ TIMESTAMP_NOW=$(date +%s.%N)
 LASTLOG_PACKETS_SENT=$(cat $RBFEEDER_LOG_FILE | grep "Packets sent in the last 30 seconds" | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
 LASTLOG_TIMESTAMP=$(date --date="$(echo $LASTLOG_PACKETS_SENT | cut -d '[' -f 1)" +%s.%N)
 LASTLOG_NUM_PACKETS_SENT=$(echo $LASTLOG_PACKETS_SENT | cut -d ']' -f 2 | cut -d ':' -f 2 | cut -d ',' -f 1 | tr -d ' ')
+echo $TIMESTAMP_NOW, $LASTLOG_PACKETS_SENT, $LASTLOG_TIMESTAMP, $LASTLOG_NUM_PACKETS_SENT
 
 # check to make sure we've sent packets in the past 60 seconds
 if [ $(echo "($TIMESTAMP_NOW - $LASTLOG_TIMESTAMP) < 60" | bc) -ne 1 ]; then
@@ -19,11 +20,10 @@ else
         EXITCODE=1
     else
         echo "At least $LASTLOG_NUM_PACKETS_SENT packets sent in past 60 seconds. HEALTHY"
+        # truncate the log file
+        truncate --size=0 /var/log/rbfeeder.log
     fi
 fi
-
-# truncate the log file
-truncate --size=0 /var/log/rbfeeder.log
 
 exit $EXITCODE
 
