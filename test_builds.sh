6a679e2080cd511886b9f6aec4f93fa3df89bae5
Mike Nye <mike.nye@gmail.com>
2020-07-23 14:32:34 +0800

    Add build test script

---
+++ test_builds.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -xe
+
+# Colours
+NOCOLOR='\033[0m'
+LIGHTPURPLE='\033[1;35m'
+
+# Save current dir
+pushd .
+
+# Test build the mlat-client deb
+echo -e "${LIGHTPURPLE}========== Test building mlat-client ==========${NOCOLOR}"
+pushd ./mlat-builder
+docker build . -t mlat-builder:latest
+
+# Copy deb out of mlat-builder
+echo -e "${LIGHTPURPLE}========== Copy mlat-client out of image ==========${NOCOLOR}"
+mkdir -p output
+rm -v ./output/*.deb || true
+MLAT_BUILDER_CONTAINER_ID=$(timeout 300s docker run -d --rm mlat-builder sleep 300)
+FILE_TO_COPY=$(docker exec "${MLAT_BUILDER_CONTAINER_ID}" bash -c "ls /src/mlat-client*.deb")
+docker cp "${MLAT_BUILDER_CONTAINER_ID}:${FILE_TO_COPY}" ./output/
+docker kill "${MLAT_BUILDER_CONTAINER_ID}"
+docker rm "${MLAT_BUILDER_CONTAINER_ID}" || true
+
+# Return to previous directory
+popd 
+
+# Build & push latest
+echo -e "${LIGHTPURPLE}========== Building ${REPO}/${IMAGE}:latest ==========${NOCOLOR}"
+docker build -t "mikenye/radarbox:testing" .
