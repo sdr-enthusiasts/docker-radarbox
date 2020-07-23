b7b6ecae4863f78c1fffe29a7f0d43f5533f87fb
Mike Nye <mike.nye@gmail.com>
2020-07-23 14:57:39 +0800

    Improve build scripts

--- build_production.sh
+++ build_production.sh
@@ -1,42 +1,35 @@
 #!/usr/bin/env bash
 set -xe
 
-git checkout master
-
 REPO=mikenye
 IMAGE=radarbox
 PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64"
+
+docker context use x86_64
+export DOCKER_CLI_EXPERIMENTAL="enabled"
+docker buildx use homecluster
 
 # Colours
 NOCOLOR='\033[0m'
 LIGHTPURPLE='\033[1;35m'
 
-# Save current dir
-pushd .
-
 # Firstly we need to build the mlat-client:armhf deb
 echo -e "${LIGHTPURPLE}========== Building mlat-client:armhf ==========${NOCOLOR}"
-docker context use arm32v7
-cd ./mlat-builder
-docker build . -t mlat-builder:latest
+pushd ./mlat-builder
+docker --context=arm32v7 build . -t mlat-builder:latest
 
 # Copy deb out of mlat-builder
 echo -e "${LIGHTPURPLE}========== Copy mlat-client:armhf out of image ==========${NOCOLOR}"
 mkdir -p output
 rm -v ./output/*.deb || true
-MLAT_BUILDER_CONTAINER_ID=$(timeout 300s docker run -d --rm mlat-builder sleep 300)
-FILE_TO_COPY=$(docker exec "${MLAT_BUILDER_CONTAINER_ID}" bash -c "ls /src/mlat-client*.deb")
-docker cp "${MLAT_BUILDER_CONTAINER_ID}:${FILE_TO_COPY}" ./output/
-docker kill "${MLAT_BUILDER_CONTAINER_ID}"
-docker rm "${MLAT_BUILDER_CONTAINER_ID}" || true
+MLAT_BUILDER_CONTAINER_ID=$(timeout 300s docker --context=arm32v7 run -d --rm mlat-builder sleep 300)
+FILE_TO_COPY=$(docker --context=arm32v7 exec "${MLAT_BUILDER_CONTAINER_ID}" bash -c "ls /src/mlat-client*.deb")
+docker --context=arm32v7 cp "${MLAT_BUILDER_CONTAINER_ID}:${FILE_TO_COPY}" ./output/
+docker --context=arm32v7 kill "${MLAT_BUILDER_CONTAINER_ID}"
+docker --context=arm32v7 rm "${MLAT_BUILDER_CONTAINER_ID}" || true
 
 # Return to previous directory
 popd 
-
-# Return to original build contexts
-docker context use x86_64
-export DOCKER_CLI_EXPERIMENTAL="enabled"
-docker buildx use homecluster
 
 # Build & push latest
 echo -e "${LIGHTPURPLE}========== Building ${REPO}/${IMAGE}:latest ==========${NOCOLOR}"
