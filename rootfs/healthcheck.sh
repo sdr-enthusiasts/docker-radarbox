a51bc7c2599877b8f817b598375941423b738dba
Mike <mike@narx.net>
2020-09-15 22:25:40 +0800

    Fix healthcheck (#33)
    * Fix healthcheck
    * Linting

--- rootfs/healthcheck.sh
+++ rootfs/healthcheck.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
 EXITCODE=0
 
+CHECK_PERIOD_SECONDS=300
+
+TIMESTAMP_NOW=$(date +%s.%N)
 LASTLOG_PACKETS_SENT=$(grep "Packets sent in the last 30 seconds" "$RBFEEDER_LOG_FILE" | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
-#LASTLOG_TIMESTAMP=$(date --date="$(echo "$LASTLOG_PACKETS_SENT" | cut -d '[' -f 2 | cut -d ']' -f 1)" +%s.%N)
+LASTLOG_TIMESTAMP=$(date --date="$(echo "$LASTLOG_PACKETS_SENT" | cut -d '[' -f 2 | cut -d ']' -f 1)" +%s.%N)
 LASTLOG_NUM_PACKETS_SENT=$(echo "$LASTLOG_PACKETS_SENT" | cut -d ']' -f 2 | cut -d ':' -f 2 | cut -d ',' -f 1 | tr -d ' ')
 
-# check to make sure we've sent packets since the last healthcheck
-if [[ "$LASTLOG_NUM_PACKETS_SENT" -lt 1 ]]; then
-    echo "No packets sent since last healthcheck. UNHEALTHY"
+# check to make sure we've sent packets recently
+if [ "$(echo "($TIMESTAMP_NOW - $LASTLOG_TIMESTAMP) < $CHECK_PERIOD_SECONDS" | bc)" -ne 1 ]; then
+    echo "No packets sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
     EXITCODE=1
 else
-    echo "At least $LASTLOG_NUM_PACKETS_SENT packets sent since last healthcheck. HEALTHY"
+    if [ "$LASTLOG_NUM_PACKETS_SENT" -lt 1 ]; then
+        echo "No packets sent in past $CHECK_PERIOD_SECONDS seconds. UNHEALTHY"
+        EXITCODE=1
+    else
+        echo "$LASTLOG_NUM_PACKETS_SENT packets sent in past 30 seconds. HEALTHY"
+    fi
 fi
-
-# wipe the log file
-truncate --size=0 "$RBFEEDER_LOG_FILE"
 
 exit $EXITCODE
 
