0995a68da894983425a54e6ea7f07ea9b378f620
Mike <mike.nye@gmail.com>
2022-02-03 20:35:47 +0800

    update healthcheck

--- rootfs/healthcheck.sh
+++ rootfs/healthcheck.sh
@@ -7,7 +7,7 @@ EXITCODE=0
 CHECK_PERIOD_SECONDS=300
 
 TIMESTAMP_NOW=$(date +%s.%N)
-LASTLOG_PACKETS_SENT=$(grep "Packets sent in the last 30 seconds" "$RBFEEDER_LOG_FILE" | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
+LASTLOG_PACKETS_SENT=$(grep "Packets sent in the last 60 seconds" "$RBFEEDER_LOG_FILE" | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
 LASTLOG_TIMESTAMP=$(date --date="$(echo "$LASTLOG_PACKETS_SENT" | cut -d '[' -f 2 | cut -d ']' -f 1)" +%s.%N)
 LASTLOG_NUM_PACKETS_SENT=$(echo "$LASTLOG_PACKETS_SENT" | cut -d ']' -f 2 | cut -d ':' -f 2 | cut -d ',' -f 1 | tr -d ' ')
 
