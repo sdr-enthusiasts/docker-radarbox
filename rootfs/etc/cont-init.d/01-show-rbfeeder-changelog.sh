935a3d473cb47c7031c813a6fb34c8f7c7c94d3b
Mike Nye <mike.nye@gmail.com>
2022-12-15 10:56:33 +0800

    shellcheck linting

--- rootfs/etc/cont-init.d/01-show-rbfeeder-changelog.sh
+++ rootfs/etc/cont-init.d/01-show-rbfeeder-changelog.sh
@@ -4,6 +4,6 @@ white="\e[0;97m"
 reset="\e[0m"
 
 # Show latest changelog
-echo -e ${white}
+echo -e "${white}"
 zcat /usr/share/doc/rbfeeder/changelog.gz | grep -B9999 -m 1 " --  "
-echo -e ${reset}
+echo -e "${reset}"
--- rootfs/usr/bin/rbfeeder_wrapper.sh
+++ rootfs/usr/bin/rbfeeder_wrapper.sh
@@ -4,11 +4,11 @@
 # All command line arguments passed to this script will be passed directly to rbfeeder_armhf.
 
 # attempt to run natively
-if $(/usr/bin/rbfeeder_armhf --no-start --version > /dev/null 2>&1); then
-    /usr/bin/rbfeeder_armhf $@
+if /usr/bin/rbfeeder_armhf --no-start --version > /dev/null 2>&1; then
+    /usr/bin/rbfeeder_armhf "$@"
 
-elif $(qemu-arm-static /usr/bin/rbfeeder_armhf --no-start --version > /dev/null 2>&1); then
-    qemu-arm-static /usr/bin/rbfeeder_armhf $@
+elif qemu-arm-static /usr/bin/rbfeeder_armhf --no-start --version > /dev/null 2>&1; then
+    qemu-arm-static /usr/bin/rbfeeder_armhf "$@"
 
 else
     >&2 echo "ERROR: Could not run rbfeeder natively or via qemu"
