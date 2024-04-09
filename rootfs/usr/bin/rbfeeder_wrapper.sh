0674d8cb706ab3a70c0f8dbc07f7da355acd0452
Matthias Wirth <matthias.wirth@gmail.com>
2024-04-09 17:29:50 +0200

    make s6-svc -r /run/services/rbfeeder work as intended
    fix non-functional grep -v

--- rootfs/usr/bin/rbfeeder_wrapper.sh
+++ rootfs/usr/bin/rbfeeder_wrapper.sh
@@ -9,12 +9,12 @@ s6wrap=(s6wrap --quiet --timestamps --prepend="$(basename "$0")" --args)
 
 # attempt to run natively
 if /usr/bin/rbfeeder_arm --no-start --version >/dev/null 2>&1; then
-    /usr/bin/rbfeeder_arm "$@"
+    exec /usr/bin/rbfeeder_arm "$@"
 
 elif qemu-arm-static /usr/bin/rbfeeder_arm --no-start --version >/dev/null 2>&1; then
-    qemu-arm-static /usr/bin/rbfeeder_arm "$@"
+    exec qemu-arm-static /usr/bin/rbfeeder_arm "$@"
 
 else
     "${s6wrap[@]}" echo "[ERROR] Could not run rbfeeder natively or via qemu"
-    sleep infinity & wait $!
+    exec sleep infinity
 fi
